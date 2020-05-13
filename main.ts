7c9e4287538972c8f9fb41599a19222fb74f68fe
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-14 02:40:48 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace progressAnimation{
     //% blockId=progressAnimationLoading
-    //% block="progressAni at $ledSayisi $ledSirasi, $durum"
+    //% block="progressAni at $ledSayisi, $ledSirasi, $durum"
     //% ledSayisi.min=1 ledSayisi.max=5
     //% ledSirasi.min=0 ledSirasi.max=4
     
@@ -8,39 +8,42 @@ namespace progressAnimation{
 
 
     export function progressAni(ledsayisi: number, sira: number, durum: boolean):void {
-     const leds=ledsayisi;
-     const ledsirasi= sira;
-     let state = durum;
+        ledsayisi |=0
+        sira |=0
+        durum ||true
+        const leds=ledsayisi;
+        const ledsirasi= sira;
+        let state = durum;
 
 
-     while (state == true) {
-        for (let satir = 0; satir <= 4; satir++) {
-            for (let sutun = 0; sutun <= ledsayisi - 1; sutun++) {
-                led.plot(satir, sutun + ledsirasi)
-                for (let index = 0; index <= 50; index++) {
-                    led.setBrightness(index * 4 + 55)
-                    basic.pause(10)
+        while (state == true) {
+            for (let satir = 0; satir <= 4; satir++) {
+                for (let sutun = 0; sutun <= ledsayisi - 1; sutun++) {
+                    led.plot(satir, sutun + ledsirasi)
+                    for (let index = 0; index <= 50; index++) {
+                        led.setBrightness(index * 4 + 55)
+                        basic.pause(10)
+                    }
+                    led.setBrightness(55)
                 }
-                led.setBrightness(55)
             }
+            for (let index = 0; index < 4; index++) {
+                led.enable(false)
+                basic.pause(100)
+                led.enable(true)
+                basic.pause(100)
+            }
+            state = false
+            led.setBrightness(255)
         }
-        for (let index = 0; index < 4; index++) {
-            led.enable(false)
-            basic.pause(100)
-            led.enable(true)
-            basic.pause(100)
-        }
-        state = false
-        led.setBrightness(255)
+        basic.pause(500)
+        basic.clearScreen()
     }
-    basic.pause(500)
-    basic.clearScreen()
+
+
+
+
 }
-
-
-
-
-    }
 
 
 
