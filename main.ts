d954da307f5ccd25b63109f5b002a17bc5d78867
Ümit UĞURLU <umitugurlu02@gmail.com>
2020-05-14 03:05:15 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 namespace progressAnimation{
     //% blockId=progressAnimationLoading
-    //% block="progressAni LedSayisi = $ledsayisi, LedSirasi =  $sira, durum = $durum"
+    //% block="progressAnim LedSayisi = $ledsayisi, LedSirasi =  $sira, durum = $durum"
     //% ledSayisi.min=1 ledSayisi.max=5
     //% ledSirasi.min=0 ledSirasi.max=4
     
